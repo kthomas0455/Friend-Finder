82db2c4677e4040de00e4ae15056fba56820cb98
Kyle Thomas <34077284+kthomas0455@users.noreply.github.com>
2018-04-25 15:40:40 -0700

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+var friends = [
+	{
+		name: "Wade Wilson",
+		photo: "https://vignette.wikia.nocookie.net/marvelmovies/images/6/66/Deadpool_promo.png/revision/latest/scale-to-width-down/280?cb=20150707202600",
+		scores:[5, 1, 1, 3, 5, 1, 2, 1, 1, 5],
+
+		name: "Jean Grey",
+		photo: "http://th.comicsblog.it/rXu5VzK_q9iZPhOXaxwcMjV9WBc=/fit-in/655xorig/http%3A%2F%2Fmedia.comicsblog.it%2Fe%2Feac%2Fmarvel-jean-gray-official-image.jpg",
+		scores:[3, 2, 5, 1, 3, 4, 2, 4, 4, 5],
+
+		name: "Logan",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC7vzKXw1JeovtedJEO5T_mi_LF1I8OvHr1uyOXPRjxNEaya7Exw",
+		scores:[1, 1, 1, 1, 2, 2, 2, 1, 1, 1],
+
+		name: "Bruce Banner",
+		photo: "http://www.popboks.com/img/film/hulk.jpg",
+		scores:[1, 1, 3, 2, 5, 2, 5, 5, 1, 3],
+
+	}
+];
+
+module.exports = friends;
